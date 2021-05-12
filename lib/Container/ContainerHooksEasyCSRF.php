d19fa7e5204c48b9a26d5b891142ecf09b65cd24
Iris System <iris@iris.ac.nz>
2021-05-13 05:50:26 +1200

    container: ContainerHooksEasyCSRF: Add hookContainerEasyCSRFSession

--- lib/Container/ContainerHooksEasyCSRF.php
+++ lib/Container/ContainerHooksEasyCSRF.php
@@ -2,8 +2,9 @@
 declare(strict_types=1);
 namespace ix\Container;
 
-// use \ix\Session\Session;
 use \ix\Container\Container;
+use \ix\Session\Session;
+use \ix\Session\EasyCSRFSessionProvider;
 use \EasyCSRF\EasyCSRF;
 
 final class ContainerHooksEasyCSRF {
@@ -21,6 +22,27 @@ final class ContainerHooksEasyCSRF {
 	public static function hookContainerEasyCSRFCookie(array $key, Container $container) {
 		$container->set('csrf', function() {
 			$session_provider = new \EasyCSRF\NativeCookieProvider();
+			return new EasyCSRF($session_provider);
+		});
+
+		return $container;
+	}
+	
+	/**
+	 * Container hook to add an EasyCSRF object as `csrf`, using Redis-backed
+	 * storage via \ix\Session\Session.
+	 *
+	 * NOTE: You must also add the Session hook (`hookContainerSession` from
+	 * `\ix\Container\ContainerHooksSession`) to the Container construction
+ 	 * for this hook to work at all.
+	 *
+	 * @param string[] $key Hook key (unused)
+	 * @param Container $container The Container instance
+	 * @return Container The Container instance
+	 */
+	public static function hookContainerEasyCSRFSession(array $key, Container $container) {
+		$container->set('csrf', function(Container $container) {
+			$session_provider = new EasyCSRFSessionProvider($container->get('session'));
 			return new EasyCSRF($session_provider);
 		});
 
