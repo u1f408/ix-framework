23e2b7e05c2a1942115d58e3df6a906f37c51833
Iris System <iris@iris.ac.nz>
2021-05-13 03:35:27 +1200

    container: ContainerHooksSession: init

---
+++ lib/Container/ContainerHooksSession.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+namespace ix\Container;
+
+use \ix\Session\Session;
+use \ix\Container\Container;
+use \EasyCSRF\EasyCSRF;
+
+final class ContainerHooksSession {
+	/**
+	 * Container hook to add an a Session instance as `session`.
+	 *
+	 * @param string[] $key Hook key (unused)
+	 * @param Container $container The Container instance
+	 * @return Container The Container instance
+	 */
+	public static function hookContainerSession(array $key, Container $container) {
+		$container->set('session', function() {
+			return new Session(null);
+		});
+
+		return $container;
+	}
+}
