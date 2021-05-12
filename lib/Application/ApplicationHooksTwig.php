da150980bd92b077db939d5621f1e9bbe5f29283
Iris System <iris@iris.ac.nz>
2021-05-13 06:42:55 +1200

    application: ApplicationHooksTwig: init

---
+++ lib/Application/ApplicationHooksTwig.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+namespace ix\Application;
+
+use \ix\Application\Application;
+use \Slim\Views\TwigMiddleware;
+
+final class ApplicationHooksTwig {
+	/**
+	 * Application middleware hook to add a `\Slim\Views\TwigMiddleware'.
+	 *
+	 * @param string[] $key Hook key (unused)
+	 * @param \Slim\App $app The App instance
+	 * @return \Slim\APp The App instance
+	 */
+	public static function hookApplicationMiddlewareTwig(array $key, \Slim\App $app): \Slim\App {
+		$app->add(TwigMiddleware::createFromContainer($app));
+		return $app;
+	}
+}
